887031f929b2af1ac7ccca2433b759d5b8d903e0
Guadalupe Delorme <guadalupedelorme@gmail.com>
2020-10-30 15:55:54 -0300

    Punto 1b visto en la consulta

--- punto1c-tp4.c
+++ punto1c-tp4.c
@@ -1,25 +1,35 @@
-#include<stdio.h>
-#define TAMA 6
+#include <stdio.h>
+#define TAMA 20
 
-int main(){
-int arre[TAMA];
+int main()
+{
+    int arre[TAMA];
+    int sumaPar = 0, sumaImpar=0;
 
-printf("Ingrese 20 numeros entereos:");
-for (int i=0; i<TAMA;i++){
-    printf("\n arre[%d]:",i);
-     scanf("%d",&arre[i]);
+    printf("Ingrese 20 numeros entereos:");
+    for (int i = 0; i < TAMA; i++)
+    {
+        printf("\n arre[%d]:", i);
+        scanf("%d", &arre[i]);
+    }
+    printf("El arreglo ingresado es: ");
+
+    for (int i = 0; i < TAMA; i++)
+    {
+        printf("\n %d", i, arre[i]);
+    }
+
+    for (int i = 0; i < TAMA ; i+=2) 
+    {
+        sumaPar += arre[i];
+        if (i+1 < TAMA)
+        {
+            sumaImpar += arre[i + 1];
+        }
+        
+    }
     
-  
-}
-printf("El arreglo ingresado es: ");
-
-for(int i=0;i<TAMA;i++){
-     
     
 
-    printf("\n %d",i,arre[i*2]);
-
-}
-
-return 0;
+    return 0;
 }
